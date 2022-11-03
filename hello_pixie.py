7be7fd35b15e58826353c69551c2572e29b36d30
rfranr <rfranr@gmail.com>
2022-11-03 06:50:11 +0100

    load two models

--- hello_pixie.py
+++ hello_pixie.py
@@ -6,13 +6,21 @@ class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
-        # Load the environment model.
-        self.scene = self.loader.loadModel("./models/character_a.glb")
+        # Load one model.
+        self.model_a = self.loader.loadModel("./models/character_a.glb")
         # Reparent the model to render.
-        self.scene.reparentTo(self.render)
+        self.model_a.reparentTo(self.render)
         # Apply scale and position transforms on the model.
-        self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(0, 14, 0)
+        self.model_a.setScale(0.25, 0.25, 0.25)
+        self.model_a.setPos(0, 14, 0)
+
+        # Load second model.
+        self.model_b = self.loader.loadModel("./models/mailbox.glb")
+        # Reparent the model to render.
+        self.model_b.reparentTo(self.render)
+        # Apply scale and position transforms on the model.
+        self.model_b.setScale(0.25, 0.25, 0.25)
+        self.model_b.setPos(-1, 8, 0)
 
 
 app = MyApp()
