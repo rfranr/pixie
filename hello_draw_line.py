61ecad43e379bc257096bda48217ec203f2e4194
rfranr <rfranr@gmail.com>
2022-11-05 14:43:13 +0100

    draw a line

--- hello_draw_line.py
+++ hello_draw_line.py
@@ -2,38 +2,39 @@ from direct.showbase.ShowBase import ShowBase
 from panda3d.core import LineSegs
 from panda3d.core import NodePath
 
+
 class MyApp(ShowBase):
 
-	def __init__(self):
-		ShowBase.__init__(self)
+    def __init__(self):
+        ShowBase.__init__(self)
 
-		ori_x = 0;
-		ori_y = 15;
-		ori_z = 0;
+        ori_x = 0
+        ori_y = 15
+        ori_z = 0
 
-		# Create Line Segs
-		self.lines_z = LineSegs()
+        # Create Line Segs
+        self.lines_z = LineSegs()
 
-		# Define one line for the z-axis. 
-		self.lines_z.moveTo(ori_x,ori_y,ori_z)
-		self.lines_z.drawTo(ori_x,ori_y,ori_z + 20)
+        # Define one line for the z-axis.
+        self.lines_z.moveTo(ori_x, ori_y, ori_z)
+        self.lines_z.drawTo(ori_x, ori_y, ori_z + 20)
 
-		# Define one line for the y-axis. 
-		self.lines_z.moveTo(ori_x,ori_y,ori_z)
-		self.lines_z.drawTo(ori_x,ori_y - 20 ,ori_z)
+        # Define one line for the y-axis.
+        self.lines_z.moveTo(ori_x, ori_y, ori_z)
+        self.lines_z.drawTo(ori_x, ori_y - 20, ori_z)
 
-		# Define one line for the x-axis. 
-		self.lines_z.moveTo(ori_x,ori_y,ori_z)
-		self.lines_z.drawTo(ori_x + 20 ,ori_y ,ori_z)
+        # Define one line for the x-axis.
+        self.lines_z.moveTo(ori_x, ori_y, ori_z)
+        self.lines_z.drawTo(ori_x + 20, ori_y, ori_z)
 
-		self.lines_z.setThickness(4)
-		self.lines_z.setColor(200,200,200)
+        self.lines_z.setThickness(4)
+        self.lines_z.setColor(200, 200, 200)
 
-		self.node_z = self.lines_z.create()
-		self.np_z = NodePath(self.node_z)
+        self.node_z = self.lines_z.create()
+        self.np_z = NodePath(self.node_z)
 
-		# Reparent node lines to render.
-		self.np_z.reparentTo(self.render)
+        # Reparent node lines to render.
+        self.np_z.reparentTo(self.render)
 
 
 app = MyApp()
